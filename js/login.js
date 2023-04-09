a24dda3db55563eb6402ba8f36c17510ad28c1ea
Keerthana <63527972+keerthanajs@users.noreply.github.com>
2023-04-09 17:18:10 +0530

    refactored login

--- js/login.js
+++ js/login.js
@@ -1,50 +1,54 @@
-$(document).ready(function(){
-    var sessionId = localStorage.getItem("sessionId");
+function login(event){
+    event.preventDefault()
+    
+    var username = $("#username").val();
+    var password = $("#password").val();
 
+    loginRequest = {
+        "username" : username, "password" : password
+    };
+
+    $.ajax({
+        url: "php/login.php",
+        type: "POST",
+        datatype: "json",
+        contentType: "application/json",
+        data: JSON.stringify(loginRequest),
+        success: function(data){
+            //console.log(JSON.stringify(data))
+            loginResponse = JSON.parse(data);
+            console.log(loginResponse);
+            if(loginResponse.success){
+                localStorage.setItem("sessionId", loginResponse.sessionId);
+                window.location.href = "profile.html"
+            }else {
+                $("#message").addClass("alert-danger");
+                $("#message").html("Invalid username or password")
+            }
+        }
+    })
+};
+
+function checkIfUserIsAlreadyLoggedIn(sessionId){
     $.ajax({
         url: "php/login.php?sessionId="+sessionId,
         type: "GET",
         datatype: "json",
         success: function(data){
-            console.log(data);
-            loginResponse = JSON.parse(data);
-            console.log(loginResponse);
-            if(loginResponse.exists){
+            activeSession = JSON.parse(data);
+            console.log(activeSession);
+            if(activeSession.exists){
                 window.location.href = "profile.html"
             }
         }
-    })
-    
-    
-    $("#loginButton").click(function(event){
-        event.preventDefault()
-        
-        var username = $("#username").val();
-        var password = $("#password").val();
+    });
+}
 
-        loginRequest = {
-            "username" : username, "password" : password
-        };
+$(document).ready(function(){
+    var sessionId = localStorage.getItem("sessionId");
 
-        $.ajax({
-            url: "php/login.php",
-            type: "POST",
-            datatype: "json",
-            contentType: "application/json",
-            data: JSON.stringify(loginRequest),
-            success: function(data){
-                //console.log(JSON.stringify(data))
-                loginResponse = JSON.parse(data);
-                console.log(loginResponse);
-                if(loginResponse.success){
-                    localStorage.setItem("sessionId", loginResponse.sessionId);
-                    window.location.href = "profile.html"
-                }else {
-                    $("#message").addClass("alert-danger");
-                    $("#message").html("Invalid username or password")
-                }
-            }
-        })
-    })
+    checkIfUserIsAlreadyLoggedIn(sessionId);
+
+    $("#loginButton").click(login);
     
-})
+});
